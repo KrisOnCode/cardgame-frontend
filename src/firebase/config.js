3917fd8e2573487aa7a8cfbc2a9797b43af67a80
Kris Paschal <krispaschal@gmail.com>
2022-06-28 12:37:21 -0500

    Update config.js
    removed fb keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBWYbhE5TYVVg5iTPyFt4ShA4-UYcwMLc",
-  authDomain: "cardgame-6ef2a.firebaseapp.com",
-  projectId: "cardgame-6ef2a",
-  storageBucket: "cardgame-6ef2a.appspot.com",
-  messagingSenderId: "647331028301",
-  appId: "1:647331028301:web:f839702eae7968f9b22b04"
+  apiKey: 
+  authDomain: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId:
 }
 
 // init firebase
